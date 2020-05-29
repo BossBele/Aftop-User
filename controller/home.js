1ad188c9c2b4078cbd236b186f738ba542d2f0ae
maricajr <maricaneston38@gmail.com>
2020-05-29 16:05:03 +0300

    large tile max likes movie

--- controller/home.js
+++ controller/home.js
@@ -5,238 +5,253 @@ let all_series = [];
 let all_movies_latest = [];
 let all_movies_action = [];
 let all_movies_horror = [];
+let max_like_movie = [];
+let max_like_movie_all = [];
 let tokens = null;
 let original_name = null;
 let LOADING_COMPLETE = false;
 let reference_movies_latest = videos.all_videos().movies_latest;
 let reference_movies_action = videos.all_videos().movies_action;
 let reference_movies_horror = videos.all_videos().movies_horror;
+let reference_movies_max_like = videos.all_videos().max_like_movie;
+let reference_movies_max_like_all = videos.all_videos().max_like_movie_all;
 let reference_series_all = videos.all_videos().series_all;
 
-exports.index = function (request, response) {
+exports.index = function(request, response) {
 
-    view_all_series(reference_series_all,all_series,tokens,original_name,response);
+  view_all_series(reference_series_all, all_series, tokens, original_name, response);
 
 };
 
-function view_all_series(reference_series_all,all_series,tokens,original_name,response) {
+function view_all_series(reference_series_all, all_series, tokens, original_name, response) {
   //for all_series
   reference_series_all.get()
-      .then((snapshot) => {
-          arrayObject(snapshot);
-      })
-      .catch((err) => {
-          console.log('Error getting documents', err);
-      });
+    .then((snapshot) => {
+      arrayObject(snapshot);
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
 
   function arrayObject(snapshot) {
-      snapshot.forEach((doc) => {
-          all_series.push(doc.data())
-      });
+    snapshot.forEach((doc) => {
+      all_series.push(doc.data())
+    });
 
-      let count = 0;
-      all_series.forEach((data) => {
-          bucket.file(data.cover_photo).get().then(function (data) {
-              bindToken(data,count,all_series);
-              count++;
-          });
+    let count = 0;
+    all_series.forEach((data) => {
+      bucket.file(data.cover_photo).get().then(function(data) {
+        bindToken(data, count, all_series);
+        count++;
       });
+    });
   }
 
-  function bindToken(data,index,all_series) {
-      const file = data[0];
-      all_series.forEach((info)=>{
-         if (info.cover_photo === file.name){
-             tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
-             original_name = file.name.split('/')[1];
-             info.tokens = tokens;
-             info.file_name = original_name;
-         }
-      });
-
-      if (all_series.length === index+1){
-          toView(response,"series",all_series);
+  function bindToken(data, index, all_series) {
+    const file = data[0];
+    all_series.forEach((info) => {
+      if (info.cover_photo === file.name) {
+        tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
+        original_name = file.name.split('/')[1];
+        info.tokens = tokens;
+        info.file_name = original_name;
       }
+    });
+
+    if (all_series.length === index + 1) {
+      toView(response, "series", all_series);
+    }
 
   }
   //end for all_series
 }
 
-function view_all_movie(reference_movies_latest,all_movies_latest,tokens,original_name,response,key) {
+function view_all_movie(reference_movies_latest, all_movies_latest, tokens, original_name, response, key) {
   //for all_series
   reference_movies_latest.get()
-      .then((snapshot) => {
-          arrayObject(snapshot);
-      })
-      .catch((err) => {
-          console.log('Error getting documents', err);
-      });
+    .then((snapshot) => {
+      arrayObject(snapshot);
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
 
   function arrayObject(snapshot) {
-      snapshot.forEach((doc) => {
-          all_movies_latest.push(doc.data())
+    snapshot.forEach((doc) => {
+      all_movies_latest.push(doc.data())
+    });
+
+    let count = 0;
+
+    if (all_movies_latest.length === 0) {
+      bindToken(null, null, all_movies_latest);
+    } else {
+      all_movies_latest.forEach((data) => {
+        bucket.file(data.cover_photo).get().then(function(data) {
+          bindToken(data, count, all_movies_latest);
+          count++;
+        });
       });
-
-      let count = 0;
-
-      if (all_movies_latest.length === 0) {
-          bindToken(null,null,all_movies_latest);
-      }else{
-          all_movies_latest.forEach((data) => {
-              bucket.file(data.cover_photo).get().then(function (data) {
-                  bindToken(data,count,all_movies_latest);
-                  count++;
-              });
-          });
-      }
+    }
 
   }
 
-  function bindToken(data,index,all_movies_latest) {
+  function bindToken(data, index, all_movies_latest) {
 
-      if (all_movies_latest.length === 0) {
-          toView(response,key,all_movies_latest);
-      }else {
-          const file = data[0];
-          all_movies_latest.forEach((info)=>{
-             if (info.cover_photo === file.name){
-                 tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
-                 original_name = file.name.split('/')[1];
-                 info.tokens = tokens;
-                 info.file_name = original_name;
-             }
-          });
+    if (all_movies_latest.length === 0) {
+      toView(response, key, all_movies_latest);
+    } else {
+      const file = data[0];
+      all_movies_latest.forEach((info) => {
+        if (info.cover_photo === file.name) {
+          tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
+          original_name = file.name.split('/')[1];
+          info.tokens = tokens;
+          info.file_name = original_name;
+        }
+      });
 
-          if (all_movies_latest.length === index+1){
-                toView(response,key,all_movies_latest);
-          }
+      if (all_movies_latest.length === index + 1) {
+        toView(response, key, all_movies_latest);
       }
+    }
 
   }
   //end for all_series
 }
 
-function toView(response,key,videos) {
+function toView(response, key, videos) {
   if (index_media.hasOwnProperty(key) === false) {
-      index_media[key] = videos;
+    index_media[key] = videos;
   }
 
   if (Object.keys(index_media).length === 1) {
 
-      LOADING_COMPLETE = false;
-      view_all_movie(reference_movies_latest,all_movies_latest,tokens,original_name,response,"movies_latest");
+    LOADING_COMPLETE = false;
+    view_all_movie(reference_movies_latest, all_movies_latest, tokens, original_name, response, "movies_latest");
 
-  }else if (Object.keys(index_media).length === 2) {
+  } else if (Object.keys(index_media).length === 2) {
 
-      LOADING_COMPLETE = false;
-      view_all_movie(reference_movies_action,all_movies_action,tokens,original_name,response,"action_movie");
+    LOADING_COMPLETE = false;
+    view_all_movie(reference_movies_action, all_movies_action, tokens, original_name, response, "action_movie");
 
-  }else if (Object.keys(index_media).length === 3) {
+  } else if (Object.keys(index_media).length === 3) {
 
-      LOADING_COMPLETE = false;
-      view_all_movie(reference_movies_horror,all_movies_horror,tokens,original_name,response,"horror_movie");
+    LOADING_COMPLETE = false;
+    view_all_movie(reference_movies_horror, all_movies_horror, tokens, original_name, response, "horror_movie");
 
-  }else{
-      LOADING_COMPLETE = true;
-      response.render('index.ejs', {
-            series_videos: index_media.series,
-            movies_videos: index_media.movies_latest,
-            movies_action_videos : index_media.action_movie,
-            movies_horror_videos : index_media.horror_movie
-      });
+  } else if (Object.keys(index_media).length === 4) {
+
+    LOADING_COMPLETE = false;
+    view_all_movie(reference_movies_max_like, max_like_movie, tokens, original_name, response, "max_like_movie");
+
+  } else if (Object.keys(index_media).length === 5) {
+
+    LOADING_COMPLETE = false;
+    view_all_movie(reference_movies_max_like_all, max_like_movie_all, tokens, original_name, response, "max_like_movie_all");
+
+  } else {
+    response.render('index.ejs', {
+      series_videos: index_media.series,
+      movies_videos: index_media.movies_latest,
+      movies_action_videos: index_media.action_movie,
+      movies_horror_videos: index_media.horror_movie,
+      movies_max_like: index_media.max_like_movie,
+      movies_max_like_all: index_media.max_like_movie_all
+    });
 
   }
 
 }
 
-exports.download = function (request, response) {
-    response.render('downloads.ejs')
+exports.download = function(request, response) {
+  response.render('downloads.ejs')
 };
 
-exports.category = function (request, response) {
-    response.render('category.ejs')
+exports.category = function(request, response) {
+  response.render('category.ejs')
 };
 
-exports.view = function (request, response) {
+exports.view = function(request, response) {
 
-    let reference = videos.series(request.params.movie_id);
-    let series = [];
-    let tokens = null;
-    let original_name = null;
-    let bucket = videos.storage;
+  let reference = videos.series(request.params.movie_id);
+  let series = [];
+  let tokens = null;
+  let original_name = null;
+  let bucket = videos.storage;
 
-    reference.get()
-        .then((snapshot) => {
-            arrayObject(snapshot);
-        })
-        .catch((err) => {
-            console.log('Error getting documents', err);
-        });
+  reference.get()
+    .then((snapshot) => {
+      arrayObject(snapshot);
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
 
-        function arrayObject(snapshot) {
-            snapshot.forEach((doc) => {
-                series.push(doc.data())
-            });
+  function arrayObject(snapshot) {
+    snapshot.forEach((doc) => {
+      series.push(doc.data())
+    });
 
-            let count = 0;
-            series.forEach((data) => {
-                bucket.file(data.trailer).get().then(function (data) {
-                    bindToken(data,count,series);
-                    count++;
-                });
-            });
+    let count = 0;
+    series.forEach((data) => {
+      bucket.file(data.trailer).get().then(function(data) {
+        bindToken(data, count, series);
+        count++;
+      });
+    });
+  }
+
+  function bindToken(data, index, series) {
+    const file = data[0];
+    series.forEach((info) => {
+      if (info.trailer === file.name) {
+        tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
+        original_name = file.name.split('/')[1];
+        info.tokens = tokens;
+        info.file_name = original_name;
+      }
+    });
+
+    if (series.length === index + 1) {
+      if (series.length !== 0) {
+
+        if (series[0].video_id) {
+          // movies
+          switch (series[0].genre) {
+            case "Action":
+              response.render('view.ejs', {
+                video: series,
+                similar_video: index_media.action_movie
+              });
+              break;
+            case "Horror":
+              response.render('view.ejs', {
+                video: series,
+                similar_video: index_media.horror_movie
+              });
+              break;
+            default:
+
+          }
+        } else {
+          // series
+          switch (series[0].genre) {
+            case "Action":
+              response.render('view.ejs', {
+                video: series,
+                similar_video: index_media.series
+              });
+              break;
+            default:
+
+          }
         }
 
-        function bindToken(data,index,series) {
-            const file = data[0];
-            series.forEach((info)=>{
-               if (info.trailer === file.name){
-                   tokens = file.metadata.metadata.firebaseStorageDownloadTokens;
-                   original_name = file.name.split('/')[1];
-                   info.tokens = tokens;
-                   info.file_name = original_name;
-               }
-            });
+      }
 
-            if (series.length === index+1){
-              if (series.length !== 0) {
+    }
 
-                if(series[0].video_id){
-                  // movies
-                  switch (series[0].genre) {
-                    case "Action":
-                        response.render('view.ejs', {
-                            video: series,
-                            similar_video: index_media.action_movie
-                        });
-                      break;
-                    case "Horror":
-                        response.render('view.ejs', {
-                            video: series,
-                            similar_video: index_media.horror_movie
-                        });
-                      break;
-                    default:
-
-                  }
-                }else {
-                  // series
-                  switch (series[0].genre) {
-                    case "Action":
-                        response.render('view.ejs', {
-                            video: series,
-                            similar_video: index_media.series
-                        });
-                      break;
-                    default:
-
-                  }
-                }
-
-              }
-
-            }
-
-        }
+  }
 
 };
--- controller/videos.js
+++ controller/videos.js
@@ -2,60 +2,72 @@ const credential = require('./connection/firebase_credential');
 const db = credential.firestore();
 exports.storage = credential.storage().bucket();
 
-exports.all_videos = function () {
-    return getVideos(db);
+exports.all_videos = function() {
+  return getVideos(db);
 };
 
-exports.series = function (series_id) {
-    let check = series_id.split('_');
+exports.series = function(series_id) {
+  let check = series_id.split('_');
 
-    if (check[0] === "movie") {
-        trailer = db.collection('movies').where('video_id','==',series_id);
-    }else{
-        trailer = db.collection('series').where('series_id','==',series_id);
-    }
+  if (check[0] === "movie") {
+    trailer = db.collection('movies').where('video_id', '==', series_id);
+  } else {
+    trailer = db.collection('series').where('series_id', '==', series_id);
+  }
 
-    return trailer;
+  return trailer;
 
 }
 
 function getVideos(db) {
 
-    let series_all;
-    let series_latest;
-    let movies_all;
-    let movies_latest;
-    let movies_action;
-    let movies_horror;
-    let data = {};
+  let series_all;
+  let series_latest;
+  let movies_all;
+  let movies_latest;
+  let movies_action;
+  let movies_horror;
+  let max_like_movie;
+  let max_like_movie_all;
+  let data = {};
 
-    movies_action = db.collection('movies')
-        .where('genre', '==', 'Action')
-        .where('launched', '==', true)
-        .orderBy('launch_date', 'desc');
+  movies_action = db.collection('movies')
+    .where('genre', '==', 'Action')
+    .where('launched', '==', true)
+    .orderBy('launch_date', 'desc');
 
-    movies_horror = db.collection('movies')
-        .where('genre', '==', 'Horror')
-        .where('launched', '==', true)
-        .orderBy('launch_date', 'desc');
+  movies_horror = db.collection('movies')
+    .where('genre', '==', 'Horror')
+    .where('launched', '==', true)
+    .orderBy('launch_date', 'desc');
 
-    movies_latest = db.collection('movies')
-        .where('launched', '==', true)
-        .orderBy('launch_date', 'desc');
+  movies_latest = db.collection('movies')
+    .where('launched', '==', true)
+    .orderBy('launch_date', 'desc');
 
-    movies_all = db.collection('movies')
-        .orderBy('launch_date', 'desc');
+  max_like_movie = db.collection('movies')
+    .orderBy('likes', 'desc')
+    .limit(1);
 
-    series_all = db.collection('series')
-        .orderBy('release_date', 'desc');
+  max_like_movie_all = db.collection('movies')
+    .orderBy('likes', 'desc')
+    .startAt(2);
+
+  movies_all = db.collection('movies')
+    .orderBy('launch_date', 'desc');
+
+  series_all = db.collection('series')
+    .orderBy('release_date', 'desc');
 
 
-    data.movies_action = movies_action;
-    data.movies_latest = movies_latest;
-    data.movies_all = movies_all;
-    data.series_all = series_all;
-    data.movies_horror = movies_horror;
+  data.movies_action = movies_action;
+  data.movies_latest = movies_latest;
+  data.movies_all = movies_all;
+  data.series_all = series_all;
+  data.movies_horror = movies_horror;
+  data.max_like_movie = max_like_movie;
+  data.max_like_movie_all = max_like_movie_all;
 
-    return data;
+  return data;
 
 }
