7d05ff70003960e51492567236105edec7311f9b
BossBele <adambeleko@gmail.com>
2020-06-12 07:05:04 +0300

    UI Changes, Added partials

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 var home = require("../controller/home");
-var client_auth = require("../controller/client_authentication")
-var video = require("../controller/videos")
+var client_auth = require("../controller/client_authentication");
+var video = require("../controller/videos");
 
 router.get("/", home.index);
 router.get("/category/:type", home.category);
@@ -11,5 +11,8 @@ router.get("/view/:movie_id/:genre", home.view);
 router.post("/signin", client_auth.signin);
 router.post("/signup", client_auth.signup);
 router.get("/watch_movie/:movie_name", video.watch_movie);
+router.get("/terms", (req, res) => {
+  res.render("terms.ejs");
+});
 
 module.exports = router;
